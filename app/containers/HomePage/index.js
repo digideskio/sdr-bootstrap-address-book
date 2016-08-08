fc350c05583b5b25aa6419f3f76510a4701cda64
Corvuscoraxpy <corvus.corax.py@gmail.com>
2016-08-08 17:05:43 +0300

    several components

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,6 +11,7 @@
 
 import React from 'react';
 import Sample from 'components/Sample';
+import CommentBox from 'components/CommentBox';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -20,6 +21,7 @@ export default class HomePage extends React.Component { // eslint-disable-line r
           <h1>Address Book</h1>
           <h3>This is a place where you should place your components</h3>
           <Sample />
+          <CommentBox />
         </div>
     );
   }
