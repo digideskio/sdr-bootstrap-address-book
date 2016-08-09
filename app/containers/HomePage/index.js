a2b69d5282ace5f0bbc44b2d50c5eb7b806595e0
Olexiy <olexiylyhun@gmail.com>
2016-08-09 13:50:17 +0300

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	app/components/CommentBox/index.js
    #	app/components/CommentForm/index.js

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,8 +11,7 @@
 
 import React from 'react';
 import Sample from 'components/Sample';
-import Simple from 'components/Simple';
-import ContactsList from 'components/ContactsList';
+
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -22,8 +21,6 @@ export default class HomePage extends React.Component { // eslint-disable-line r
           <h1>Address Book</h1>
           <h3>This is a place where you should place your components</h3>
           <Sample />
-            <Simple />
-            <ContactsList />
         </div>
     );
   }
