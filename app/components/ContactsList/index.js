246f26cbdb5308094385d78098c3fae6b467eb6a
Olexiy <olexiylyhun@gmail.com>
2016-08-09 11:32:34 +0300

    add new components

---
+++ app/components/ContactsList/index.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import Contact from 'components/Contact';
+
+var CONTACTS = [
+    {
+        id: 1,
+        name: 'Darth Vader',
+        phoneNumber: '+250966666666',
+        image: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif'
+    }, {
+        id: 2,
+        name: 'Princess Leia',
+        phoneNumber: '+250966344466',
+        image: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif'
+    }, {
+        id: 3,
+        name: 'Luke Skywalker',
+        phoneNumber: '+250976654433',
+        image: 'http://images5.fanpop.com/image/photos/25300000/Luke-ROTJ-gif-luke-skywalker-25357298-250-107.gif'
+    }, {
+        id: 4,
+        name: 'Chewbacca',
+        phoneNumber: '+250456784935',
+        image: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif'
+    }
+];
+
+
+
+var ContactsList = React.createClass({
+    getInitialState: function() {
+        return {
+            displayedContacts: CONTACTS
+        };
+    },
+
+    handleSearch: function(event) {
+        var searchQuery = event.target.value.toLowerCase();
+        var displayedContacts = CONTACTS.filter(function(el) {
+            var searchValue = el.name.toLowerCase();
+            return searchValue.indexOf(searchQuery) !== -1;
+        });
+
+        this.setState({
+            displayedContacts: displayedContacts
+        });
+    },
+
+    render: function() {
+        return (
+            <div>
+                <input type="text" placeholder="name" className="form-control" onChange={this.handleSearch} />
+                <ul className="list-group">
+                    {
+                        this.state.displayedContacts.map(function(el) {
+                            return <Contact
+                                key={el.id}
+                                name={el.name}
+                                phoneNumber={el.phoneNumber}
+                                image={el.image}
+                            />;
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+});
+
+export default ContactsList;
