dec9ce5c550d7c1695e2e1c32336e7db8f670ff1
Olexiy <olexiylyhun@gmail.com>
2016-08-09 21:33:57 +0300

    add edit comment

--- app/components/CommentForm/index.js
+++ app/components/CommentForm/index.js
@@ -5,12 +5,20 @@ class CommentForm extends Component {
     super(props);
     this.state = {
       author: '',
-      text: ''
+      text: '',
+      edit: 0
     };
 
     this.handleAuthorChange = this.handleAuthorChange.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.edit['id'] !== 0) {
+      this.setState({author: nextProps.edit['author']});
+      this.setState({text: nextProps.edit['text']});
+    }
   }
 
   handleAuthorChange(e) {
@@ -28,28 +36,28 @@ class CommentForm extends Component {
     if (!text || !author) {
       return;
     }
-    this.props.onUpdate(author,text);
-    this.setState({author: '', text: ''});
+    this.props.onUpdate(author,text,this.props.edit['id']);
+    this.setState({author: '', text: '', edit: {author:'',text:'',edit:0}});
   }
 
   render() {
-    return(
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your name"
-          value={this.state.author}
-          onChange={this.handleAuthorChange}
-        />
-        <input
-          type="text"
-          placeholder="Say something..."
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-        <input type="submit" value="Post" />
-      </form>
-    );
+      return (
+          <form className="commentForm" onSubmit={this.handleSubmit}>
+            <input
+                type="text"
+                placeholder="Your name"
+                value={this.state.author}
+                onChange={this.handleAuthorChange}
+            />
+            <input
+                type="text"
+                placeholder="Say something..."
+                value={this.state.text}
+                onChange={this.handleTextChange}
+            />
+            <input type="submit" value="Post"/>
+          </form>
+      );
   }
 
 }
