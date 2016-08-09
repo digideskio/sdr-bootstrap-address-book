91547d6ad8a4181e496be184f2268f3d8b36d2fe
Dmytro Medzatiy <d.medzatiy@gmail.com>
2016-08-09 16:34:43 +0300

    function rename & initial /id/ value generated

--- app/components/CommentBox/index.js
+++ app/components/CommentBox/index.js
@@ -2,23 +2,34 @@ import React, {Component} from 'react';
 import CommentList from 'components/CommentList';
 import CommentForm from 'components/CommentForm';
 
-var arr = [
+const arr = [
   {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"},
+  {id: 5, author: "Samuel Jeckson", text: "Hello another one comment"}
 ];
+
+function nextIndex() {
+
+    let id;
+    if (!arr.length) {return 0; }
+    else { const id_arr = arr.map((x) => {return x.id});
+      id = Math.max(...id_arr) }
+    return id+1;
+  }
 
 class CommentBox extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-                  data: arr,
-                  id_n: 3
-                  };
-    this.onNewComment = this.onNewComment.bind(this);
+      data: arr,
+      id_n: nextIndex()
+    };
+    this.onAddComment = this.onAddComment.bind(this);
   }
 
-  onNewComment(author,text) {
+  onAddComment(author,text) {
 
     const newComment = { id: this.state.id_n,
                           author: author,
@@ -34,7 +45,7 @@ class CommentBox extends Component {
       <div className="CommentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm onUpdate={this.onNewComment} />
+        <CommentForm onUpdate={this.onAddComment} />
 
       </div>
     );
