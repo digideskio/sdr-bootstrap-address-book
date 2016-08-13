7490dc9eb1c485810513225b6e6be47d113ada5e
olexiy <olexiylyhun@gmail.com>
2016-08-13 19:08:38 +0300

    add a possibility to create new channels

--- app/components/ChatSwitcher/index.js
+++ app/components/ChatSwitcher/index.js
@@ -4,8 +4,22 @@
 
 import React, {PropTypes, Component} from 'react';
 import ChatSwitcherElement from 'components/ChatSwitcherElement';
+import Input from 'components/Input';
 
 export default class ChatSwitcher extends Component {
+
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const title = this.inputForTitle.getValue().trim();
+        if (!title) {
+            alarm('Please enter title');
+            return;
+        }
+        //const channel = Object.assign({}, this.props.editableComment, {author, text});
+        this.props.onAddNewChanel(title);
+    };
 
     render() {
         const {currentChat, chatList, onChatSwitch} = this.props;
@@ -36,12 +50,32 @@ export default class ChatSwitcher extends Component {
           width: 'strech',
           backgroundColor: '#f1f1f1'
         };
+
+        const styleSpan = {
+            backgroundColor:'#4d394b',
+            color:'#fcfcfc',
+            borderColor:'#4d394b'
+        };
+
         return (
           <div>
             <h2 className="text-center text-uppercase"
                 style={headerStyle}>
                 Select channel
             </h2>
+              <form className="commentForm"
+                    onSubmit={this.handleSubmit} >
+                  <div className="input-group">
+                      <Input ref={me => this.inputForTitle = me}
+                             placeholder="Title of channel"
+                             type="text"
+                             value={""}
+                             className="form-control" />
+                      <span className="input-group-addon" style={styleSpan}>
+            <Input type="submit" value="Add" />
+          </span>
+                  </div>
+              </form>
             <ul style={listStyle}>
               {chats}
             </ul>
