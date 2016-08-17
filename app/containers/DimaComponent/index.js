4be002f768ff4daca376f89712118070172fcd06
Dmytro Medzatiy <d.medzatiy@gmail.com>
2016-08-17 21:03:30 +0300

    TodoApp counters

---
+++ app/containers/DimaComponent/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by DMedzatiy on 17-Aug-16.
+ */
+
+import React, { Component } from 'react';
+
+class DimaComponent extends Component {
+    render(){
+        return (
+            <div className="container">
+                
+            </div>    
+        )
+    }
+}
