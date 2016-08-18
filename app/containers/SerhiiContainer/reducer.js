fe35c1ea2b5d593837c57afa3cfa7026f1963596
Corvuscoraxpy <corvus.corax.py@gmail.com>
2016-08-18 12:56:22 +0300

    reducer changed

--- app/containers/SerhiiContainer/reducer.js
+++ app/containers/SerhiiContainer/reducer.js
@@ -11,7 +11,10 @@ function addTodoSerhii(state = initialState, action) {
   switch (action.type) {
     case "ADD_TODO":
       list.push(action.text)
-      return Object.assign({}, state, {countOfTodo: action.id, listOfTodo: list});
+      return Object.assign({}, state, {
+        countOfTodo: action.id,
+        listOfTodo: list
+      });
 
     default:
       return state;
