7e49f36b546bec2cc21961fcb2f2b526d6697513
Corvuscoraxpy <corvus.corax.py@gmail.com>
2016-08-22 15:10:14 +0300

    refractoring CommentForm, added classnames package for joining classNames together

--- app/components/CommentForm/index.js
+++ app/components/CommentForm/index.js
@@ -1,51 +1,64 @@
 import React, { Component, PropTypes } from 'react';
 import Input from 'components/Input';
+import classNames from 'classnames/bind';
+import styles from './styles.css';
+
+
+let commentStyles = classNames.bind(styles);
 
 export default class CommentForm extends Component {
 
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const author = this.inputForAuthor.getValue().trim();
-    const text = this.inputForText.getValue().trim();
-    if (!text || !author) {
-      return;
-    }
-    const comment = Object.assign({}, this.props.editableComment, {author, text});
-    this.props.onSaveUpdate(comment);
-  };
-
-  render() {
-    const {editableComment} = this.props;
-    const styleSpan = {
-      backgroundColor: '#4d394b',
-      color: '#fcfcfc',
-      borderColor: '#4d394b'
+    handleSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const author = this.inputForAuthor.getValue().trim();
+        const text = this.inputForText.getValue().trim();
+        if (!text || !author) {
+          return;
+        }
+        const comment = Object.assign({}, this.props.editableComment, {author, text});
+        this.props.onSaveUpdate(comment);
     };
-    return (
-        <form className="commentForm"
-              onSubmit={this.handleSubmit}>
-          <div className="input-group" style={{margin: '0 20px 0 20px'}}>
-            <Input ref={me => this.inputForAuthor = me}
-                   placeholder="Yor name"
-                   type="text"
-                   value={editableComment.author}
-                   className="form-control"/>
-            <span className="input-group-addon" style={styleSpan}>&</span>
-            <Input ref={me => this.inputForText = me}
-                   placeholder="Say something..."
-                   type="text"
-                   value={editableComment.text}
-                   className="form-control"/>
-          <span className="input-group-addon" style={styleSpan}>
-             <Input type="submit" value="Post"/>
-          </span>
-          </div>
-        </form>
-    );
-  }
 
+    render() {
+        const {editableComment} = this.props;
+        const spanStyle = commentStyles({
+            'span-colors':true,
+            'input-group-addon':true
+        });
+
+
+        return (
+            <form
+                className="commentForm"
+                onSubmit={this.handleSubmit}>
+                <div className="input-group">
+                    <Input
+                        ref={me => this.inputForAuthor = me}
+                        placeholder="Yor name"
+                        type="text"
+                        value={editableComment.author}
+                        className="form-control"
+                    />
+                    <span className={spanStyle}>&</span>
+                    <Input
+                        ref={me => this.inputForText = me}
+                        placeholder="Say something..."
+                        type="text"
+                        value={editableComment.text}
+                        className="form-control"
+                    />
+                    <span className={spanStyle}>
+                        <Input
+                            type="submit"
+                            value="Post"
+                        />
+                    </span>
+                </div>
+            </form>
+        );
+    }
 }
 
 CommentForm.propTypes = {
